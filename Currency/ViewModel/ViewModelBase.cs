baffea39a5f83520dce042862ff356ed336a5bf6
kalugin <nabupalassar@gmail.com>
2017-10-18 21:52:08 +0300

    Prymenko Part

---
+++ Currency/ViewModel/ViewModelBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        Organization _selectedOrganization;
+        public Organization SelectedOrganization
+        {
+            get { return _selectedOrganization; }
+            set
+            {
+                if (_selectedOrganization == value)
+                    return;
+                _selectedOrganization = value;
+                OnPropChanged("SelectedOrganization");
+            }
+        }
+        ObservableCollection<Organization> _organization;
+        public ObservableCollection<Organization> Organizations
+        {
+            get
+            {
+                if (_organization == null)
+                    _organization = AutoSalon.GetCars();
+                return _cars;
+            }
+            set
+            {
+                _cars = value;
+                OnPropChanged("Cars");
+            }
+        }
+
+    }
+}
