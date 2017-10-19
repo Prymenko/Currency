2ed4a035b90056ddea7d372e3fba43dac3013eb3
gallifrrey1886 <gallifrrey1886@outlook.com>
2017-10-19 13:07:16 +0300

    добавлены классы для работы с входящим XML

---
+++ Currency/Data/Currencies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency.Data
+{
+    public class Currencies
+    {
+        public string id { get; set; }
+
+        public double br { get; set; }
+
+        public double ar { get; set; }
+    }
+}
---
+++ Currency/Data/Organizations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency.Data
+{
+    public class Organizations
+    {
+        public string name { get; set; }
+
+        public string region { get; set; }
+
+        public string city { get; set; }
+
+        public string phone { get; set; }
+
+        public string adress { get; set; }
+
+        public string link { get; set; }
+
+        public List<Currencies> currencies { get; set; }
+    }
+}
